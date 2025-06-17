1d1c8f10206c688c641e040c40e58f95fe089a26
Neutronicals <124219367+Neutronicals@users.noreply.github.com>
2025-06-17 13:13:57 +0000

    Update youtube-downloader.ts
    Just updating thats all hehehe

--- src/app/youtube-downloader/youtube-downloader.ts
+++ src/app/youtube-downloader/youtube-downloader.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-youtube-downloader',
-  imports: [],
   templateUrl: './youtube-downloader.html',
-  styleUrl: './youtube-downloader.css'
+  styleUrl: './youtube-downloader.css',
+  standalone: true,
+  imports: []
 })
 export class YoutubeDownloader {
+  youtubeUrl: string = '';
+  info: any = null;
+  selectedFormat: string = '';
+  apiBase = 'http://localhost:8000/api'; // Change if deploying elsewhere
 
+  constructor(private http: HttpClient) {}
+
+  fetchInfo() {
+    if (!this.youtubeUrl) return;
+    this.http.get(`${this.apiBase}/info`, { params: { url: this.youtubeUrl } })
+      .subscribe(data => {
+        this.info = data;
+        if (this.info.formats && this.info.formats.length > 0) {
+          this.selectedFormat = this.info.formats[0].format_id;
+        }
+      });
+  }
+
+  download() {
+    if (!this.youtubeUrl || !this.selectedFormat) return;
+    const url = `${this.apiBase}/download?url=${encodeURIComponent(this.youtubeUrl)}&format_id=${this.selectedFormat}`;
+    window.open(url, '_blank');
+  }
 }
