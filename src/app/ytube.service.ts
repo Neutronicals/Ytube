6b11effcd6f3350890ae5bfb09b637f54c29bc14
Neutronicals <124219367+Neutronicals@users.noreply.github.com>
2025-06-25 08:52:17 +0000

    Create ytube.service.ts

---
+++ src/app/ytube.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({ providedIn: 'root' })
+export class YtubeService {
+  // Change the URL below to your Render backend URL (no trailing slash)
+  backendUrl = 'https://YOUR_RENDER_URL.onrender.com';
+
+  constructor(private http: HttpClient) {}
+
+  getVideoInfo(url: string): Observable<any> {
+    return this.http.get<any>(`${this.backendUrl}/api/info`, {
+      params: new HttpParams().set('url', url),
+    });
+  }
+
+  downloadVideo(url: string, formatId: string = 'best'): Observable<Blob> {
+    return this.http.get(`${this.backendUrl}/api/download`, {
+      params: new HttpParams().set('url', url).set('format_id', formatId),
+      responseType: 'blob',
+    });
+  }
+}
